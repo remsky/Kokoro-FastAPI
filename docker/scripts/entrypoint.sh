b93af0c846b1d3fbc7e4068ced9126e2fafd4b3e
faltiska <alfred.faltiska@outlook.com>
2025-08-27 11:59:44 +0300

    Reverted unwanted change

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 
-exec uv run --extra $DEVICE --no-sync python -m uvicorn api.src.main:app --host 0.0.0.0 --port 8880 --log-level $UVICORN_LOG_LEVEL --workers $NUM_WORKERS
+if [ "$DOWNLOAD_MODEL" = "true" ]; then
+    python download_model.py --output api/src/models/v1_0
+fi
+
+exec uv run --extra $DEVICE --no-sync python -m uvicorn api.src.main:app --host 0.0.0.0 --port 8880 --log-level debug
